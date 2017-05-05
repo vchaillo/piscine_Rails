16c287dfaba4fb3d4e4fd0610026d61f23f0aa96
vchaillo <valentinchaillou89@gmail.com>
2017-05-05 15:06:54 +0200

    update ex01 d01 to one line

--- d01/ex01/croissant.rb
+++ d01/ex01/croissant.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby -w
 
 def croissant
-  file = open('numbers.txt', 'r').read
-  array = file.split(',').map(&:to_i)
-  puts array.sort
+  puts open('numbers.txt', 'r').read.split(',').map(&:to_i).sort
 end
 
 croissant if $PROGRAM_NAME == __FILE__
